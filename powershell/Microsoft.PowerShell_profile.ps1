ef91c2bf408d0e7fcefc98c508ff9d6c079ba9ce
soregashi.expo70 <soregashi.expo70@gmail.com>
2023-06-25 06:09:03 +0900

    Add feature show platform and user role.

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,9 +2,25 @@ Set-PSReadlineOption -EditMode Emacs
 Import-Module posh-git
 $GitPromptSettings.DefaultPromptSuffix = $('`n> ' * ($nestedPromptLevel + 1))
 function prompt {
+    # Detect current platform powershell running on and user role, because of show these.
+    $currentPlatformRunningOn = '';
+    $currentUserRole = '';
+    if ($PSVersionTable.Platform = 'UNIX') {
+        $currentPlatformRunningOn = '[UNIX]';
+    } else {
+        $currentPlatformRunningOn = '[WIN]';
+
+        $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+        $principal = [Security.Principal.WindowsPrincipal] $identity
+        $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
+        $currentUserRole = if($principal.Equals($adminRole)) { '[ADMIN]' } else { '[]' }
+    }
+
     # Your non-prompt logic here
-    $prompt = Write-Prompt "PS:"
-    $prompt += Write-Prompt "[$(Get-Date -f 'yyyy-MM-ddTHH:mm:ss%K')] " -ForegroundColor ([ConsoleColor]::White)
+    $prompt = $currentPlatformRunningOn + $currentUserRole
+    $prompt += Write-Prompt "PS:"
+    $prompt += "[$(Get-Date -f 'yyyy-MM-ddTHH:mm:ss%K')] "
     $prompt += & $GitPromptScriptBlock
     if ($prompt) { "$prompt" } else { " " }
 }
